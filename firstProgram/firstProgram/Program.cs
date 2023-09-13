81dc646c6090a6f9d0dac90f6b6fd05582c76c07
MikkelHebel <mikkelhebelv2@gmail.com>
2023-09-13 09:36:58 +0200

    Updated some code in program.cs

--- firstProgram/firstProgram/Program.cs
+++ firstProgram/firstProgram/Program.cs
@@ -1,38 +1,20 @@
-﻿namespace firstProgram
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace firstProgram
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            string? name;
-            int age;
+            string? str = "";
 
-            // Ask for name
-            Console.WriteLine("Input name: ");
-            name = Console.ReadLine();
-
-            // Ask for age
-            Console.WriteLine("Input age: ");            
-            // Continuesly check for age until a valid input is given
-            while (true) {
-                if (int.TryParse(Console.ReadLine(), System.Globalization.NumberStyles.Integer, null, out age))
-                    break;
-                Console.WriteLine("Invalid input! Try again.");
-            }
-
-            if (12 > age)
-                Console.WriteLine("Et barn");
-            else if (19 >= age)
-                Console.WriteLine("En teenager");
-            else if (25 >= age)
-                Console.WriteLine("En studerende");
-            else if (67 >= age)
-                Console.WriteLine("I arbejde");
-            else
-                Console.WriteLine("En pensionist");
-
-            Console.WriteLine("Hello {0}, you are {1} years old.", name, age);
-            Console.WriteLine("In 120 years you will be {0} years old.", age+120);
+            do
+            {
+                str = Console.ReadLine();
+            } while (!(str.Length > 5));
+            Console.WriteLine(str[3]);
+            Console.WriteLine(str[2]);
+            Console.WriteLine(str[4]);
             Console.ReadLine();
         }
     }
